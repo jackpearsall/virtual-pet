0ee9b97f224bd86e47416c91b6ba02967688d7e5
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-02-25 21:18:09 +0000

    Completed keeping fit functionality

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -8,4 +8,49 @@ describe('constructor', () => {
     const pet = new Pet('Fido');
     expect(pet.name).toEqual('Fido');
   });
- });
+  it('expect the pet to have an initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
+  });
+  it('expect the pet to have an initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+  });
+  it('expect the pet to have an initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
+describe('growUp', () => {
+  it('increases age by 1 year', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  });
+  it('increases hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+  it('decreases fitness by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
+});
+
+describe('walk', () => {
+  it('increases fitness by 4', () => {
+    const pet = new Pet('Fido');
+    pet.fitness = 0;
+    pet.walk();
+    pet.fitness = 4;
+  });
+  it('increases fitness to a max of 10', () => {
+    const pet = new Pet('Fido)');
+    pet.fitness = 9;
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,24 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
-    this.name = name;
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = MAXIMUM_FITNESS;
 }
+
+Pet.prototype.growUp = function growUp() {
+  this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
+};
+
+Pet.prototype.walk = function walk() {
+  if ((this.fitness + 4) < MAXIMUM_FITNESS) {
+    this.fitness += 4;
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
+};
 
 module.exports = Pet;
